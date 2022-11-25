995100472cffe964841295bb33a19cc9cd09beb0
Taki Eddine RAHAL <taki-eddine.rahal@proxym-it.com>
2022-11-25 14:26:17 +0100

    Reimplementation Cart And Order

--- src/main/java/com/takirahal/srfgroup/modules/home/repositories/TopHomeSlidesImageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.takirahal.srfgroup.modules.home.repositories;
-
-import com.takirahal.srfgroup.modules.home.entities.TopHomeSlidesImage;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TopHomeSlidesImageRepository extends JpaRepository<TopHomeSlidesImage, Long> {
-}
