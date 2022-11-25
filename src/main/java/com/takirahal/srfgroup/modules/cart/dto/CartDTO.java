a714644cf79b758c74b900a1ced735ba6e42a66c
Taki Eddine RAHAL <taki-eddine.rahal@proxym-it.com>
2022-11-25 15:45:54 +0100

    Deploiement

---
+++ src/main/java/com/takirahal/srfgroup/modules/cart/dto/CartDTO.java
@@ -0,0 +1,27 @@
+package com.takirahal.srfgroup.modules.cart.dto;
+
+import com.takirahal.srfgroup.modules.cart.entities.Order;
+import com.takirahal.srfgroup.modules.offer.dto.SellOfferDTO;
+import com.takirahal.srfgroup.modules.user.dto.UserDTO;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Set;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CartDTO implements Serializable {
+
+    private Long id;
+    private int quantity;
+    private Double total;
+    private String status;
+    private Instant passedDate;
+    private SellOfferDTO sellOffer;
+    private UserDTO user;
+    // private Set<OrderDTO> orders;
+}
