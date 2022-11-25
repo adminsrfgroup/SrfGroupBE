140471c3bbc368c7cea84ed9d807fd1af7ca35ac
Taki Eddine RAHAL <taki-eddine.rahal@proxym-it.com>
2022-11-25 16:56:11 +0100

    Deploiement Prod

--- src/main/java/com/takirahal/srfgroup/modules/websocket/services/WebSocketService.java
+++
@@ -1,8 +0,0 @@
-package com.takirahal.srfgroup.modules.websocket.services;
-
-import java.security.Principal;
-import java.util.List;
-
-public interface WebSocketService {
-    List<Principal> getAllConnectedUsers();
-}
