e2be388bd2b505fe1434ef4a7c8778e458a9a3f0
Taki Eddine RAHAL <taki-eddine.rahal@proxym-it.com>
2022-11-25 15:08:07 +0100

    Deploiement

--- src/main/java/com/takirahal/srfgroup/restclient/RestTemplateClientProvider.java
+++ src/main/java/com/takirahal/srfgroup/restclient/RestTemplateClientProvider.java
@@ -12,38 +12,35 @@ import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
-// @Component
+@Component
 public class RestTemplateClientProvider extends RestTemplateClient {
 
-//    @Autowired
-//    private RestTemplate restTemplate;
+    @Autowired
+    private RestTemplate restTemplate;
 
     @Value("${dynamicsvariables.base-url-backend}")
     private String baseUrl;
 
     @Override
     public <T, R> ResponseEntity<R> postHttpRequest(String url, T request, ParameterizedTypeReference<R> typeReference, HttpHeaders heades) {
-//        HttpEntity<T> requestEntity = new HttpEntity(request, heades);
-//        ResponseEntity<R> responseEntity = restTemplate.exchange(baseUrl.concat(url), HttpMethod.POST, requestEntity, typeReference);
-//        return responseEntity;
-        return null;
+        HttpEntity<T> requestEntity = new HttpEntity(request, heades);
+        ResponseEntity<R> responseEntity = restTemplate.exchange(baseUrl.concat(url), HttpMethod.POST, requestEntity, typeReference);
+        return responseEntity;
     }
 
     @Override
     public <R> ResponseEntity<R> getHttpRequest(String url, ParameterizedTypeReference<R> typeReference, List<HttpHeaders> headers) {
 
-//        HttpHeaders httpHeaders = new HttpHeaders();
-//        if( headers!= null ){
-//            headers.stream().forEach(head -> {
-//                // httpHeaders;
-//            });
-//        }
-//
-//        HttpEntity<String> httpEntity = new HttpEntity(httpHeaders);
-//        ResponseEntity<R> responseEntity = restTemplate.exchange(baseUrl.concat(url), HttpMethod.GET, httpEntity, typeReference);
-//        return responseEntity;
+        HttpHeaders httpHeaders = new HttpHeaders();
+        if( headers!= null ){
+            headers.stream().forEach(head -> {
+                // httpHeaders;
+            });
+        }
 
-        return null;
+        HttpEntity<String> httpEntity = new HttpEntity(httpHeaders);
+        ResponseEntity<R> responseEntity = restTemplate.exchange(baseUrl.concat(url), HttpMethod.GET, httpEntity, typeReference);
+        return responseEntity;
     }
 
     @Override
